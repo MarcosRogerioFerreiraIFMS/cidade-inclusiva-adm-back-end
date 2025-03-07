158477a574f5a437170a1ffd04e8ea2bd496e036
Kevin Souza <kevinwillyansouza@gmail.com>
2025-03-07 11:41:19 -0400

    feat: configurar servidor express

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(
+    `🚀 Servidor rodando na porta ${port}. Acesse em http://localhost:${port}`
+  );
+});

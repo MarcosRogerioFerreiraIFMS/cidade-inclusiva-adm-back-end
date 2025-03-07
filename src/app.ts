48e08b1f0ace5e0304f60c65724e2283594d307e
Kevin Souza <kevinwillyansouza@gmail.com>
2025-03-07 12:03:55 -0400

    feat: adicionar configuração do ESLint e Prettier ao projeto

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import 'dotenv/config';
-import express from 'express';
+import 'dotenv/config'
+import express from 'express'
 
-const app = express();
-const port = process.env.PORT || 5555;
+const app = express()
+const port = process.env.PORT || 5555
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(
     `🚀 Servidor rodando na porta ${port}. Acesse em http://localhost:${port}`
-  );
-});
+  )
+})

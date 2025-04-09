609753d1b120dc2e0fe2f6651d7b4a4f902bd517
Kevin Souza <kevinwillyansouza@gmail.com>
2025-04-09 17:21:32 -0400

    feat: atualizar dados de categorias e notícias no seed, incluindo novas categorias e publicações relevantes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -69,22 +69,55 @@ async function main() {
     // 6. CategoriaNoticia
     await tx.categoriaNoticia.deleteMany()
     await tx.categoriaNoticia.createMany({
-      data: Array.from({ length: 3 }).map((_, i) => ({
-        nome: [`Mobilidade Urbana`, `Acessibilidade`, `Eventos Inclusivos`][i]
-      }))
+      data: [
+        { nome: 'Acessibilidade' },
+        { nome: 'Inclusão' },
+        { nome: 'Educação para PCD' },
+        { nome: 'Tecnologia Assistiva' },
+        { nome: 'Direitos das PCD' },
+        { nome: 'Saúde para PCD' },
+        { nome: 'Esportes Adaptados' },
+        { nome: 'Cultura Inclusiva' }
+      ]
     })
+
     const categoriasNoticia = await tx.categoriaNoticia.findMany()
 
     // 7. Notícias (referenciam CategoriaNoticia)
     await tx.noticia.deleteMany()
     await tx.noticia.createMany({
-      data: categoriasNoticia.map((c: any) => ({
-        titulo: `Nova rampa de acesso na Av. Paulista`,
-        conteudo: `Instalação de rampa facilita acesso de cadeirantes.`,
-        url: `https://www.cidadeinclusiva.com.br/noticias/`,
-        foto: `https://source.unsplash.com/random`,
-        categoriaId: c.id
-      }))
+      data: [
+        {
+          titulo:
+            'Carro com isenção para PCD: STF entra na discussão e pode definir novas regras',
+          conteudo:
+            'A Associação Nacional de Apoio às Pessoas com Deficiência (ANAPcD) decidiu agir. Por isso, entrou com uma ação no Supremo Tribunal Federal (STF) contra mudanças que afetam diretamente os PCDs.',
+          url: 'https://www.terra.com.br/mobilidade/carro-com-isencao-para-pcd-stf-entra-na-discussao-e-pode-definir-novas-regras,4b003cb0fd9df721122d6cb3b57c1062l4x9yr3e.html',
+          foto: 'https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2024/07/08/carro-pcd-1ibjskggydg0y.jpg',
+          dataPublicacao: '2025-04-09T00:00:00.000Z',
+          categoriaId: categoriasNoticia[4].id
+        },
+        {
+          titulo:
+            'Chevrolet aumenta desconto para PCD em abril de 2025; veja nova tabela de preços',
+          conteudo:
+            'A Chevrolet do Brasil traz sua campanha para vendas diretas destinadas a pessoas com deficiência (PCD) no mês de abril de 2025 com reduções que podem chegar a R$ 41.995. E esse benefício é trazido na versão topo de linha Premier d Tracker, que utiliza com motorização 1.2 turbo e câmbio automático.',
+          url: 'https://mundodoautomovelparapcd.com.br/chevrolet-para-pcd-em-abril-de-2025/',
+          foto: 'https://mundodoautomovelparapcd.com.br/wp-content/uploads/2025/01/Chevrolet-Tracker-Premier-2025.jpg',
+          dataPublicacao: '2025-04-09T00:00:00.000Z',
+          categoriaId: categoriasNoticia[4].id
+        },
+        {
+          titulo:
+            'Abertas Inscrições para PCD em Hospitais Públicos do Pará – Confira as Vagas!',
+          conteudo:
+            'O Centro Integrado de Inclusão e Reabilitação (CIIR), em Belém; o Hospital Metropolitano de Urgência e Emergência (HMUE), em Ananindeua; e o Hospital Regional Público do Marajó (HRPM), em Breves, estão com processos seletivos abertos para contratação de profissionais em diversas áreas. As oportunidades incluem vagas exclusivas ou prioritárias para Pessoas com Deficiência (PCD).',
+          url: 'https://diariodopara.com.br/concursos-e-empregos/abertas-inscricoes-para-pcd-em-hospitais-publicos-do-para-confira-as-vagas/',
+          foto: 'https://uploads.diariodopara.com.br/2025/04/DZMTBsBu-5593_f6f999f8-70c2-289d-0103-0760237c4022-1024x682.webp',
+          dataPublicacao: '2025-04-08T00:00:00.000Z',
+          categoriaId: categoriasNoticia[5].id
+        }
+      ]
     })
 
     // 8. CategoriaProfissional
